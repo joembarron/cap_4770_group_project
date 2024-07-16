e93a9290b11f9682b872c5c4bde21db301e33471
Griffin Balducci <GriffinBalducci@outlook.com>
2024-07-16 02:15:54 -0400

    STARTED ON FRONTEND
    - React w/ Vite and Chakra UI Set-Up
    - Color themes added
    - Chakra card added
    - Input field and rating tabs added

---
+++ BookRecEngine/src/App.tsx
@@ -0,0 +1,35 @@
+﻿import { Flex} from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, Heading, Input} from '@chakra-ui/react'
+import { Tabs, TabList, Tab, VStack } from '@chakra-ui/react'
+
+function App() {
+
+    return (
+        <>
+            <Flex h='100%' w='100%' bg='#5E503F' justify='center' align='center'>
+                <Card h='500px' w='500px' align='center'>
+                  <CardHeader>
+                    <Heading size='md'>Magical Book Recommender</Heading>
+                  </CardHeader>
+
+                  <CardBody w='75%' gap='20px'>
+                    <VStack spacing='10px'>
+                        <Input placeholder='What book have you read?'/>
+                        <Tabs variant='soft-rounded' justify='center' align='center'>
+                            <TabList>
+                            <Tab _selected={{ background: '#EAE0D5' }}>💩</Tab>
+                            <Tab _selected={{ background: '#EAE0D5' }}>😒</Tab>
+                            <Tab _selected={{ background: '#EAE0D5' }}>😐</Tab>
+                            <Tab _selected={{ background: '#EAE0D5' }}>🙂</Tab>
+                            <Tab _selected={{ background: '#EAE0D5' }}>😃</Tab>
+                            </TabList>
+                        </Tabs>
+                    </VStack>
+                  </CardBody>
+                </Card>
+            </Flex>
+        </>
+    )
+}
+
+export default App
